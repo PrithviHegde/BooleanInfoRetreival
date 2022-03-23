701675d3f6690d5663859475405960e9bbcd3f6e
Kavya <cookiekavya12@gmail.com>
2022-03-23 13:03:00 +0530

    update pdoc

--- DocNames.py
+++ DocNames.py
@@ -1,5 +1,15 @@
 ''' 
 Boolean Information Retreival System
+
+There are the following features / pre-processing steps:
+
+1. Stopword Removal: Removes the common stop words from the corpus.
+
+2. Lemmatization: Employs for normalisation.
+
+3. Wildcard Query Handling: Using bi-gram
+
+4. Spelling Correction: Edit Distance Method/ Levenshtein Distance used to correct misspelled words.
  
 Query priority is as given in the following example
 
